b3662f2abc2056cd9fb14157387ac7e5b6c958e6
trigremm <trigremm@gmail.com>
2014-08-27 15:40:30 +0600

    2nd commit

---
+++ fizzbuzz.r
@@ -0,0 +1,20 @@
+for (i in 1:100){
+    if (i%%3 == 0 && i%%5 == 0) { print('FizzBuzz') } else 
+    if (i%%3 == 0)              { print('Fizz')     } else 
+    if (i%%5 == 0)              { print('Buzz')     } else 
+                                { print(i)          }
+}
+
+if (FALSE){
+x = 100
+y = 3
++ x
+- x
+x + y
+x - y
+x * y
+x / y
+x ^ y
+x %% y
+x %/% y
+}
---
+++ fizzbuzz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {1..100}
+do
+    let j=$i%3
+    let k=$i%5
+    if [ $j -eq 0 ] && [ $k -eq 0 ] 
+    then echo "FizzBuzz"
+    elif [ $j -eq 0 ]
+    then echo "Fizz"
+    elif [ $k -eq 0 ]
+    then echo "Buzz"
+    else echo $i
+    fi
+done
