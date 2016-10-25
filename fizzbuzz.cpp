3981893003cbde6bb49a0bb7e284f51ea6ea8f85
trigremm <me@trigremm.com>
2016-10-25 11:14:37 +0600

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -6,14 +6,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+
+void fizzbuzz () {
    for (int i = 1; i <= 100; i++){
-       if      (i % 3 == 0 && i % 5 == 0) cout << "FizzBuzz";
-       else if (i % 3 == 0)               cout << "Fizz";
-       else if (i % 5 == 0)               cout << "Buzz";
-       else                               cout << i;
+       if      (i%3 == 0 && i%5 == 0) cout << "FizzBuzz";
+       else if (i%3 == 0)             cout << "Fizz";
+       else if (i%5 == 0)             cout << "Buzz";
+       else                           cout << i;
        cout << endl;
    }
-   return 0;
+}
+
+
+int main(){
+    fizzbuzz ();   
+    return 0;
 }
