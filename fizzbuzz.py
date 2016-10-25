83b5263bfcd560344a7ce882148676e495ca6696
trigremm <me@trigremm.com>
2016-10-25 11:06:40 +0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,8 +3,12 @@ usage:
 python3 fizzbuzz.py
 '''
 
-for i in range(1,101):
-    if   i%3 == 0 and i%5 == 0 : print('FizzBuzz')
-    elif i%3 == 0              : print('fizz')
-    elif i%5 == 0              : print('buzz')
-    else                       : print(i)
+def fizzbuzz():
+    for i in range(1,101):
+        if   i%3 == 0 and i%5 == 0 : print('FizzBuzz')
+        elif i%3 == 0              : print('Fizz')
+        elif i%5 == 0              : print('Buzz')
+        else                       : print(i)
+
+if __name__ == '__main__':
+    fizzbuzz()
