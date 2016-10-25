677810fec2efccbeb04867abc372b893c0d52158
trigremm <me@trigremm.com>
2016-10-25 11:10:33 +0600

    Update fizzbuzz.sh

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -2,16 +2,20 @@
 # usage:
 # bash fizzbuzz.sh
 
-for i in {1..100}
-do
-    let j=$i%3
-    let k=$i%5
-    if [ $j -eq 0 ] && [ $k -eq 0 ] 
-    then echo "FizzBuzz"
-    elif [ $j -eq 0 ]
-    then echo "Fizz"
-    elif [ $k -eq 0 ]
-    then echo "Buzz"
-    else echo $i
-    fi
-done
+function fizzbuzz () {
+    for i in {1..100}
+    do
+        let j=$i%3
+        let k=$i%5
+        if [ $j -eq 0 ] && [ $k -eq 0 ] 
+        then echo "FizzBuzz"
+        elif [ $j -eq 0 ]
+        then echo "Fizz"
+        elif [ $k -eq 0 ]
+        then echo "Buzz"
+        else echo $i
+        fi
+    done
+}
+
+fizzbuzz
